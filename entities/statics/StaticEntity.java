22d11748d68d48d46877d4652ea160b25c92f86b
cadysolberg <57415166+cadysolberg@users.noreply.github.com>
2020-04-09 12:00:06 -0500

    Add files via upload

--- entities/statics/StaticEntity.java
+++ entities/statics/StaticEntity.java
@@ -7,7 +7,7 @@ import dev.tilegame.tiles.Tile;
 public abstract class StaticEntity extends Entity {
 
 	public StaticEntity(Handler handler, float x, float y, int width, int height) {
-		super(handler, x, y, Tile.TILEWIDTH, Tile.TILEHEIGHT);
+		super(handler, x, y, width, height);
 	}
 	
 
