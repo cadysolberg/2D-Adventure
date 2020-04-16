d53a7e8a94dee9b00a0fafce82e5f677e4dca035
cadysolberg <57415166+cadysolberg@users.noreply.github.com>
2020-04-16 10:36:53 -0500

    Add files via upload

--- entities/statics/Tree.java
+++ entities/statics/Tree.java
@@ -1,5 +1,6 @@
 package dev.tilegame.entities.statics;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import dev.tilegame.Handler;
@@ -11,10 +12,11 @@ public class Tree extends StaticEntity{
 	public Tree(Handler handler, float x, float y) {
 		super(handler, x, y, Tile.TILEWIDTH*2, Tile.TILEHEIGHT*2);
 		
-		bounds.x = 10;
+		bounds.x = 35;
 		bounds.y = (int)(height/1.5f);
-		bounds.width = width - 20;
+		bounds.width = width - 80;
 		bounds.height = (int)(height - height /1.5f);
+		
 	}
 
 
@@ -26,6 +28,12 @@ public class Tree extends StaticEntity{
 	@Override
 	public void render(Graphics g) {	
 		g.drawImage(Assets.tree, (int)(x - handler.getGameCamera().getxOffset()), (int)(y - handler.getGameCamera().getyOffset()), width, height, null);
+	
+		g.setColor(Color.red);
+		g.drawRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()),
+				(int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
 	}
+	
+	
 
 }
