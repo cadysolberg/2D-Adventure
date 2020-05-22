38b4644e322fd8805d74cd1dcc767e4048973b3b
cadysolberg <57415166+cadysolberg@users.noreply.github.com>
2020-05-22 10:45:11 -0500

    Add files via upload

--- entities/statics/Rock.java
+++ entities/statics/Rock.java
@@ -1,9 +1,11 @@
 package dev.tilegame.entities.statics;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import dev.tilegame.Handler;
 import dev.tilegame.gfx.Assets;
+import dev.tilegame.items.Item;
 import dev.tilegame.tiles.Tile;
 
 public class Rock extends StaticEntity{
@@ -16,10 +18,19 @@ public class Rock extends StaticEntity{
 	public void tick() {
 		
 	}
+	
+	public void die() {
+		handler.getWorld().getItemManager().addItem(Item.rockItem.createNew((int) x, (int) y));
+	}
 
 	@Override
 	public void render(Graphics g) {	
 		g.drawImage(Assets.rock, (int)(x - handler.getGameCamera().getxOffset()), (int)(y - handler.getGameCamera().getyOffset()), width, height, null);
+		if (health < 10 ) {
+			g.setColor(Color.red);
+			g.drawRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()),
+					(int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+		}
 	}
 	
 }
--- entities/statics/Tree.java
+++ entities/statics/Tree.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 
 import dev.tilegame.Handler;
 import dev.tilegame.gfx.Assets;
+import dev.tilegame.items.Item;
 import dev.tilegame.tiles.Tile;
 
 public class Tree extends StaticEntity{
@@ -12,9 +13,9 @@ public class Tree extends StaticEntity{
 	public Tree(Handler handler, float x, float y) {
 		super(handler, x, y, Tile.TILEWIDTH*2, Tile.TILEHEIGHT*2);
 		
-		bounds.x = 35;
+		bounds.x = 25;
 		bounds.y = (int)(height/1.5f);
-		bounds.width = width - 80;
+		bounds.width = width - 60;
 		bounds.height = (int)(height - height /1.5f);
 		
 	}
@@ -24,14 +25,20 @@ public class Tree extends StaticEntity{
 	public void tick() {
 		
 	}
+	
+	public void die() {
+		handler.getWorld().getItemManager().addItem(Item.woodItem.createNew((int) x, (int) y));
+	}
 
 	@Override
 	public void render(Graphics g) {	
 		g.drawImage(Assets.tree, (int)(x - handler.getGameCamera().getxOffset()), (int)(y - handler.getGameCamera().getyOffset()), width, height, null);
-	
+		if (health < 10 ) {
 		g.setColor(Color.red);
 		g.drawRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()),
 				(int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+		
+		}
 	}
 	
 	
