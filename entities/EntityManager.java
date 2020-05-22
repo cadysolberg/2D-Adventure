eee196bcecd5aa979364ff5687c678e68d0e62f1
cadysolberg <57415166+cadysolberg@users.noreply.github.com>
2020-05-22 10:30:47 -0500

    Add files via upload

--- entities/EntityManager.java
+++ entities/EntityManager.java
@@ -3,6 +3,7 @@ package dev.tilegame.entities;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 
 import dev.tilegame.Handler;
 import dev.tilegame.entities.creatures.Player;
@@ -34,11 +35,12 @@ public class EntityManager {
 	}
 	
 	public void tick() {
-		for(int i = 0; i < entities.size(); i++) {
-			Entity e = entities.get(i);
+		Iterator<Entity> it = entities.iterator();
+		while(it.hasNext()) {
+			Entity e = it.next();
 			e.tick();
 			if(!e.active) {
-				entities.remove(e);
+				it.remove();
 			}
 		}
 		entities.sort(rendering);
